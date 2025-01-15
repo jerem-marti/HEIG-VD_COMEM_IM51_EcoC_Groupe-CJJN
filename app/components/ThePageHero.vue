dce92878cfabfdd176e250d86659c9845eccdf00
jerem-marti <jeremy_martin@bluewin.ch>
2025-01-15 20:23:25 +0100

    feat(hero): the page hero created

---
+++ app/components/ThePageHero.vue
@@ -0,0 +1,53 @@
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+    title: {
+        type: String,
+        required: true,
+    },
+    bgSvgUrlEncode: {
+        type: String,
+        required: false,
+    },
+});
+
+const bgImgStyle = props.bgSvgUrlEncode ? `url("${props.bgSvgUrlEncode}")` : 'none';
+console.log(bgImgStyle);
+
+</script>
+
+<template>
+    <div class="page-title">
+        <h1>{{ $props.title }}</h1>
+        <slot></slot>
+    </div>
+</template>
+
+<style scoped>
+.page-title {
+    display: flex;
+    width: 100%;
+    /* height: 70dvh; */
+    height: auto;
+    max-height: 50rem;
+    padding: 12dvw 10dvw;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 5rem;
+    overflow: hidden;
+    background-image: v-bind(bgImgStyle);
+    background-size: cover;
+    background-position: center;
+}
+@media (max-width: 768px) {
+    .page-title {
+        padding: 20dvh 5dvw;
+    }
+}
+
+.page-title h1 {
+    text-align: center;
+}
+</style>
