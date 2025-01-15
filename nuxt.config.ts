a75970ea8a1cfaa470e48e6a1a0b5cdfef062c04
jerem-marti <jeremy_martin@bluewin.ch>
2025-01-15 17:15:07 +0100

    feat: add autoprefixer for css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   // https://nuxt.com/modules
-  modules: ['@nuxthub/core', '@nuxt/eslint', 'nuxt-svgo'],
+  modules: ['@nuxthub/core', '@nuxt/eslint', 'nuxt-svgo', '@nuxt/image'],
 
   // https://devtools.nuxt.com
   devtools: { enabled: true },
